370a5a577f11353d7aca577f94675fd9f80e70ce
alaa <alaa.alnous@gmail.com>
2022-10-12 13:16:29 +0200

    First java file

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args){
+        // System.out.println("Hello World");
+
+int score = 90;
+double score2 = 100;
+float floatScore = 12.8f;
+boolean hasScore = true;
+String name = "Alaa Ali";
+
+System.out.println(name + " has Scored " + score2 );
+
+
+    }
+
+
+}
