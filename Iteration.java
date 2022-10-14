96e64b0dfa995697f26deedb9477e89a798ddc96
alaa <alaa.alnous@gmail.com>
2022-10-14 13:33:07 +0200

    add  Iteration file

---
+++ Iteration.java
@@ -0,0 +1,33 @@
+// Iteration And loop
+
+// For loop 
+public class Iteration {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if ( i%2 == 0 ) {
+                System.out.println("odd No" +" "+i) ;
+
+            }
+
+
+        }
+        for (int i = 1; i < 20; i++) {
+            if ( i%2 == 1 ) {
+                System.out.println("even No"+ " "+i) ;
+
+            }
+
+
+        }
+//While 
+
+int startPoint = 1;
+
+         while (startPoint<6) {
+            System.out.println("Hello");
+            startPoint = startPoint+1;
+            
+            
+         }
+    }
+}
