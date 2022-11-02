ab70935111ff922dd9fe3f8af905e11f300a420c
alaa <alaa.alnous@gmail.com>
2022-11-02 11:46:12 +0200

    create constructor

---
+++ Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+    
+    String name= "";
+    int age= 0;
+
+
+    static void bark(){
+System.out.println("Barking");
+    }
+void run(){
+        System.out.println("running");
+
+    }
+
+}
---
+++ TryConst.java
@@ -0,0 +1,20 @@
+public class TryConst {
+    
+    int x;
+    int y;
+
+
+    public TryConst(int x) {
+    this.x = x;
+
+    }
+
+    /**
+     * @param x
+     * @param y
+     */
+    public TryConst(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
